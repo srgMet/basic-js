be4112ffaa1e29627e75fd3de9ccf0a70321e715
srgMet <100075467+srgMet@users.noreply.github.com>
2022-05-01 00:13:12 +0300

    feat: make some task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+  calculateDepth( arr ) {
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+  
+        for (let i = 0; i < arr.length; i++){
+             let deep = 1;
+         if (Array.isArray(arr[i]))
+            deep +=1;
+            calculateDepth(arr[i])
+         }
+      return deep
   }
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,71 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+   // throw new NotImplementedError('Not implemented');
+   // remove line with error and write your code
+   if (!Array.isArray(arr)) { throw new Error('\'arr\' parameter must be an instance of the Array!') };
+   let ar = arr.slice();
+   for (let i = 0; i < ar.length; i++) {
+      if (typeof (ar[i]) === 'string' || typeof (ar[i]) === 'number') {
+
+         if (ar[0] == ('--discard-prev')) {
+            ar.splice(0, 1)
+         };
+         if (ar[0] == ('--double-prev')) {
+            ar.splice(0, 1)
+         };
+
+         if (ar[i] == ('--discard-next') && ar[i + 2] == ('--double-prev')) {
+            ar.splice(i, 3)
+         }
+         if (ar[i] == ('--discard-next') && ar[i + 2] == ('--discard-prev')) {
+            ar.splice(i, 3)
+         }
+
+         if (ar[ar.length - 1] == ('--discard-next')) {
+            ar.splice(-1, 1)
+         };
+         if (ar[ar.length - 1] == ('--double-next')) {
+            ar.splice(-1, 1)
+         };
+
+
+         if (ar[i] == ('--discard-next')) {
+            ar.splice(i, 2);
+         };
+
+         if (ar[i] == ('--discard-prev')) {
+            ar.splice(i - 1, 2)
+         };
+
+         if (ar[i] == ('--double-next')) {
+            ar[i] = ar[i + 1];
+         };
+         if (ar[i] == ('--double-prev')) {
+            ar[i] = ar[i - 1];
+         }
+      }
+   }
+
+   return ar;
+
+
+
+
 }
 
 module.exports = {
-  transform
+   transform
 };
